3d003d394a642a253b08bea4fa8b8a89efbbc6ff
Irina <tsvetkovskaya-irina@mail.ru>
2021-09-30 12:00:41 +0300

    Initial commit

---
+++ src/main/java/com/javatechie/crud/example/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.javatechie.crud.example.service;
+
+import com.javatechie.crud.example.entity.Product;
+import com.javatechie.crud.example.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> saveProducts(List<Product> products) {
+        return productRepository.saveAll(products);
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product getProductByName(String name) {
+        return productRepository.findByName(name);
+    }
+
+    public String deleteProduct(int id) {
+        productRepository.deleteById(id);
+        return "Product removed! " + id;
+    }
+
+    public Product updateProduct(Product product) {
+        Product existingProduct = productRepository.findById(product.getId()).orElse(null);
+        existingProduct.setName(product.getName());
+        existingProduct.setQuantity(product.getQuantity());
+        existingProduct.setPrice(product.getPrice());
+        return productRepository.save(existingProduct);
+    }
+}
